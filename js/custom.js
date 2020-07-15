8981c906f7d64a615fd58c9fc408a8457a4f5772
Sweta Shree <sweta.shree@tredence.com>
2020-07-15 19:43:57 +0530

    second tab UI

--- js/custom.js
+++ js/custom.js
@@ -13,11 +13,29 @@ $(function () {
         $('#pickedDate').html(pickedDate);
     });
 
-    const rangeSlider = document.querySelector("input");
-    const value = document.querySelector(".rangevalue");
-    value.textContent = rangeSlider.value;
+    // Range Slider
+    $(document).ready(function(){
+        $('.single-slider').jRange({
+            from: 0,
+            to: 100,
+            step: 10,
+            scale: [0,10,20,30,40,50,60,70,80,90,100],
+            format: '%s',
+            width: 450,
+            showLabels: true,
+            snap: true
+        });
+    });
 
-    rangeSlider.oninput = function(){
-    	value.textContent = this.value;
-    }
+    // Range Slider
+    $(function() {
+        $("[name=recommendnumber]").click(function(){
+                $('.recommendnumber').hide();
+                $("#hide-"+$(this).val()).show('slow');
+        });
+        $("#enableexpectedearnings").click(function(){
+            $('.expectedearnings').show('slow');
+    });
+     });
+
 });
