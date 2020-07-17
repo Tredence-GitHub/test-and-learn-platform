7da8d9d47c3ba58477af9698658e9576510c28bf
Sweta Shree <sweta.shree@tredence.com>
2020-07-17 17:13:10 +0530

    login screen

--- js/custom.js
+++ js/custom.js
@@ -52,12 +52,17 @@ $(function () {
     $("#actualtestsites").click(function () {
       $(".samplecalc").show("slow");
     });
+
+    $("[name=noofzone]").click(function () {
+      $("#noofzone-2").hide();
+      $("#noofzone-" + $(this).val()).show("slow");
+    });
   });
 
 // Datatable 
   $(function() {
     $(document).ready(function() {
-      $('#zonelist').DataTable({
+      $('.zonelist').DataTable({
         "paging":   false,
         "info":     false,
         "searching" : false
@@ -120,8 +125,91 @@ $(function () {
     });
   });
 
+  $(document).ready(function () {
+    $("#boot-multiselect4").multiselect({
+      nonSelectedText: 'Select from custom set',
+      includeSelectAllOption: true,
+      numberDisplayed: 1,
+      enableFiltering: true,
+      templates: {
+        li: '<li><a class="dropdown-item"><label class="p-2"></label></a></li>',
+        ul: ' <ul class="multiselect-container dropdown-menu p-3 m-0  dark-bg"></ul>',
+        button: '<button type="button" class="multiselect dropdown-toggle text-left w100" data-toggle="dropdown" data-flip="true"><span class="multiselect-selected-text"></span> <b class="fa fa-angle-down"></b></button>',
+        filter: '<li class="multiselect-item filter"><div class="input-group m-0 p-0"><input class="form-control m-0 multiselect-search" type="text"></div></li>',
+        filterClearBtn: '<span class="input-group-btn"><button class="btn multiselect-clear-filter" type="button"><i class="fa fa-remove"></i></button></span>'
+    },
+    buttonContainer: '<div class="dropdown" />',
+    buttonClass: 'btn btn-outline-theme'
+    });
+  });
+
   $('.dropdown-menu').find('form').click(function (e) {
     e.stopPropagation();
   });
+
+// Login Slider
+let $slides, interval, $selectors, $btns, currentIndex, nextIndex;
+
+let cycle = index => {
+  let $currentSlide, $nextSlide, $currentSelector, $nextSelector;
+
+  nextIndex = index !== undefined ? index : nextIndex;
+
+  $currentSlide = $($slides.get(currentIndex));
+  $currentSelector = $($selectors.get(currentIndex));
+
+  $nextSlide = $($slides.get(nextIndex));
+  $nextSelector = $($selectors.get(nextIndex));
+
+  $currentSlide.removeClass("active").css("z-index", "0");
+
+  $nextSlide.addClass("active").css("z-index", "1");
+
+  $currentSelector.removeClass("current");
+  $nextSelector.addClass("current");
+
+  currentIndex =
+  index !== undefined ?
+  nextIndex :
+  currentIndex < $slides.length - 1 ?
+  currentIndex + 1 :
+  0;
+
+  nextIndex = currentIndex + 1 < $slides.length ? currentIndex + 1 : 0;
+};
+
+$(() => {
+  currentIndex = 0;
+  nextIndex = 1;
+
+  $slides = $(".slide");
+  $selectors = $(".selector");
+  $btns = $(".btn");
+
+  $slides.first().addClass("active");
+  $selectors.first().addClass("current");
+
+  interval = window.setInterval(cycle, 6000);
+
+  $selectors.on("click", e => {
+    let target = $selectors.index(e.target);
+    if (target !== currentIndex) {
+      window.clearInterval(interval);
+      cycle(target);
+      interval = window.setInterval(cycle, 6000);
+    }
+  });
+
+  $btns.on("click", e => {
+    window.clearInterval(interval);
+    if ($(e.target).hasClass("prev")) {
+      let target = currentIndex > 0 ? currentIndex - 1 : $slides.length - 1;
+      cycle(target);
+    } else if ($(e.target).hasClass("next")) {
+      cycle();
+    }
+    interval = window.setInterval(cycle, 6000);
+  });
+});
 
 });
